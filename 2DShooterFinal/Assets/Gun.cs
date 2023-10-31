3da51a1c3b88b62d62741f2f2b030fa65f3f3e03
Cocoloco2 <kristofferhn@hotmail.com>
2023-10-31 21:45:40 +0100

    Merge uupdate

---
+++ 2DShooterFinal/Assets/Gun.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+    
+{
+    public Rigidbody2D rb;
+
+    public Camera cam;
+    public float moveSpeed = 5f; 
+
+    Vector2 mousePos;
+
+    //Vector2 movement = spiller 
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+      
+        Vector2 dir = mousePos - rb.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        Debug.Log(dir);
+    }
+}
