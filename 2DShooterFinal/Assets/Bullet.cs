6799966790e2e980bd48d74b40ca3b223c645d0b
MadsHolt <holten06@hotmail.com>
2023-11-07 10:14:44 +0100

    Hearts

--- 2DShooterFinal/Assets/Bullet.cs
+++ 2DShooterFinal/Assets/Bullet.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject hitEffect;
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 5f);
-        Destroy(gameObject);
-    }
+    /*    public GameObject hitEffect;
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 5f);
+            Destroy(gameObject);
+        }
+    */
 
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            coll.gameObject.GetComponent<PlayerHealth>().TakeDamage(1);
+        }
+    }
 
 }
---
+++ 2DShooterFinal/Assets/Heartbar.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.UIElements;
+using static UnityEditor.Progress;
+
+public class Heartbar : MonoBehaviour
+{
+    public GameObject heartPrefab;
+    public PlayerHealth playerHealth;
+
+    //public int health = 2;
+    //public int maxhealth = 4;
+    List<heart_script> hearts = new List<heart_script>();
+
+    private void OnEnable()
+    {
+        PlayerHealth.OnPlayerDamaged += DrawHearts;
+    }
+
+    private void OnDisable()
+    {
+        PlayerHealth.OnPlayerDamaged -= DrawHearts;
+    }
+
+
+    private void Start()
+    {
+        DrawHearts();
+    }
+
+    public void DrawHearts()
+    {
+        ClearHearts();
+
+        //Determine how many hearts to make total 
+        // based off the max health
+        float maxHealthRemainder = playerHealth.maxhealth % 2;
+        int heartsToMake = (int)((playerHealth.maxhealth / 2) + maxHealthRemainder);
+        for(int i = 0; i < heartsToMake; i++) 
+        {
+            CreateEmptyHeart();
+        }
+
+        for(int i =0; i<hearts.Count; i++)
+        {
+            int heartStatusRemainder = (int)Mathf.Clamp(playerHealth.health - (i * 2), 0, 2);
+            hearts[i].SetHeartImage((HeartStatus)heartStatusRemainder);
+        }
+    }
+
+
+    public void CreateEmptyHeart()
+    {
+        
+        GameObject newHeart = Instantiate(heartPrefab);
+        newHeart.transform.SetParent(transform, false);
+
+        heart_script heartComponent = newHeart.GetComponent<heart_script>();
+        heartComponent.SetHeartImage(HeartStatus.Empty);
+        hearts.Add(heartComponent);
+        //newHeart.transform.localScale = new Vector2(2, 2);
+        
+    }
+
+    public void ClearHearts()
+    {
+        foreach(Transform t in transform)
+        {
+            Destroy(t.gameObject);
+        }
+        hearts = new List<heart_script> ();
+    }
+
+
+
+}
