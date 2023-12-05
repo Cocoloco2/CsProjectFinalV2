628f92dc8d3e7be7856ec810c84761beaff35ddb
MadsHolt <holten06@hotmail.com>
2023-12-05 11:06:17 +0100

    PG layout

--- 2DShooterFinal/Assets/WeaponParent.cs
+++ 2DShooterFinal/Assets/WeaponParent.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class WeaponParent : MonoBehaviour
 {
     public Vector2 PointerPosition { get; set; }
+    public int Index {get; set;}
+    //[SerializeField] private InputActionReference PointerPosition;
+
     
     private void Update()
     {
         //makes sure the object always spawn at the mouse position
+        //Vector3 test = PointerPosition.action.ReadValue<Vector2>();
+        //Debug.Log(test);
+        
         Vector2 direction = (PointerPosition - (Vector2)transform.position).normalized;
         transform.right = direction;
 
@@ -24,5 +31,15 @@ public class WeaponParent : MonoBehaviour
             scale.y = 1;
         }
         transform.localScale = scale;
+        
     }
+    /*
+    private Vector2 getPointerInput()
+    {
+        Vector3 mousePos = PointerPosition.action.ReadValue<Vector2>();
+        mousePos.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    */
 }
